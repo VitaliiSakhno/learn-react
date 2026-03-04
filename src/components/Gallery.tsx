49896cf1e97259ac578edf71f147fefdd2aa325f
VitaliiSakhno <vitalik.tarasovich@gmail.com>
2026-03-04 13:22:45 +0100

    replaced a couple of images

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -7,8 +7,8 @@ export default function Gallery() {
     <h1 className={css.title}>The great scientists</h1>
     <div className={css.scientists}>
       <Scientist src="https://i.imgur.com/MK3eW3As.jpg" alt="Alla Burds" name="Alla Burds"/>
-      <Scientist src="https://i.imgur.com/MK3eW3As.jpg" alt="Tiana Carol" name="Tiana Carol"/>
-      <Scientist src="https://i.imgur.com/MK3eW3As.jpg" alt="Lucid Fee" name="Lucid Fee"/>
+      <Scientist src="https://i.imgur.com/QIrZWGIs.jpg" alt="Alan L. Hart" name="Alan L. Hart"/>
+      <Scientist src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" name="Katsuko Saruhashi"/>
       </div>
     </>
   );
